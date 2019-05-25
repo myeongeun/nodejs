37dd6713b6250d5a177101d4a2511493d14bc9b3
mye0201 <mye0201@naver.com>
2019-05-25 19:00:14 +0900

    init node modules

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-
-
-
+app.get('/test', function (req, res) {
+  res.send('Hello World! yo!');
+});
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
